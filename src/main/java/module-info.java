af2075d54b9bedd3964ee8e9e1d6e68c24d1bf94
arbnoradragaj <arbnora.dragaj@student.uni-pr.edu>
2024-05-02 00:29:55 +0200

    DBConnector

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.studentaccommodation {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     exports app;
---
+++ src/main/java/services/DBConnector.java
@@ -0,0 +1,25 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private static String URL = "jdbc:mysql://localhost:3306/StudentAccommodation";
+    private static String USER = "root";
+    private static String PASSWORD = "root";
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            try {
+                connection = DriverManager.getConnection(
+                        URL, USER, PASSWORD
+                );
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
+}
