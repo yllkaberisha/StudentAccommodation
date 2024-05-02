94b7d8a54095ddd3e4cae8c71a9986ecb4b156c5
RinesaGajtani <rinesa.gajtani@student.uni-pr.edu>
2024-05-02 10:20:49 +0200

    navigator

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,14 @@
 package app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Navigator.navigate(stage, Navigator.LOGIN_PAGE);
+    }
 }
